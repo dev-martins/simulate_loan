d4a0a830eaa5a264aad0ff0afec0412c578a3394
cmartins123 <claudio.martins@gmail.com.br>
2021-06-12 14:54:03 -0300

    calcular valor da parcela

--- app/Http/Controllers/SimulationsController.php
+++ app/Http/Controllers/SimulationsController.php
@@ -22,25 +22,7 @@ class SimulationsController extends Controller
 
     public function SimulationCredit(ValidateRequest $request)
     {
-        $rate_institutions = $this->getData('taxas_instituicoes.json');
-
-        $dataKeys = array_unique(array_column($rate_institutions, 'instituicao'));
-        $keys = [];
-
-        foreach ($dataKeys as $key => $dataKey) {
-            array_push($keys, $dataKey);
-        }
-
-        $response = [];
-        foreach ($keys as $key_k => $value_key) {
-
-            foreach ($rate_institutions as $key => $value) {
-
-                if ($value_key == $value['instituicao'])
-                    $response[$value_key][] = $value;
-            }
-        }
-
+        $response = $this->getResponseSimulation($request);
         return response()->json($response, 200);
     }
 }
--- app/Traits/FileJsonTrait.php
+++ app/Traits/FileJsonTrait.php
@@ -22,5 +22,43 @@ trait FileJsonTrait
         else
             return false;
     }
-    
+
+    public function getResponseSimulation($request)
+    {
+        $rate_institutions = $this->getData('taxas_instituicoes.json');
+        $keys = $this->getKeyResponse($rate_institutions);
+        return $this->getbodyResponse($keys, $rate_institutions, $request);
+    }
+
+    protected function getKeyResponse($rate_institutions)
+    {
+        $dataKeys = array_unique(array_column($rate_institutions, 'instituicao'));
+        $keys = [];
+
+        foreach ($dataKeys as $key => $dataKey) {
+            array_push($keys, $dataKey);
+        }
+
+        return $keys;
+    }
+
+    protected function getbodyResponse($keys, $rate_institutions, $request)
+    {
+        $response = [];
+        foreach ($keys as $key_k => $value_key) {
+
+            foreach ($rate_institutions as $key => $value) {
+
+                if ($value_key == $value['instituicao'])
+                    $calc = [
+                        "taxa" => $value['taxaJuros'],
+                        "parcelas" => $value['parcelas'],
+                        "valor_parcela" => (float) number_format($value['coeficiente'] * $request->input('valor_emprestimo'), 2, '.', ''),
+                        "convenio" => $value['convenio']
+                    ];
+                $response[$value_key][] = $calc;
+            }
+        }
+        return $response;
+    }
 }
